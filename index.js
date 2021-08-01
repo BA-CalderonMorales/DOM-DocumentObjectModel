88e95157f62064356232e0b68b0dd060cd19e92c
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-01 15:28:02 -0500

    Added final images from Udemy course module.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Javascript should always go right above the closing body tag inside
 // of the index.
+// LOGIC
 document.querySelector("h1").innerHTML = "Good Bye";
+document.querySelector("#list a").style.color = "red";
+document.querySelector(".btn").style.backgroundColor = "lightblue";
+document.querySelector(".btn").style.borderRadius = "20%";
